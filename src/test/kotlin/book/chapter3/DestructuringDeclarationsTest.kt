98b4c23d78d2c315e29e60d4e748188553c25b8b
Christian Brunotte <cb@lathspell.de>
2018-05-09 00:16:39 +0200

    chapter 3

--- src/test/kotlin/book/chapter3/DestructuringDeclarationsTest.kt
+++ src/test/kotlin/book/chapter3/DestructuringDeclarationsTest.kt
@@ -2,7 +2,6 @@ package book.chapter3
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
 
 class DestructuringDeclarationsTest {
     data class MyResult(val code: Int, val msg: String)
@@ -12,16 +11,24 @@ class DestructuringDeclarationsTest {
         val (code, msg) = MyResult(32, "Foo")
         assertEquals(32, code)
         assertEquals("Foo", msg)
+
+        // the convention functions
+        val result = MyResult(32, "Foo")
+        assertEquals(32, result.component1())
+        assertEquals("Foo", result.component2())
     }
 
     @Test
     fun testMap() {
         val myMap = hashMapOf("a" to 1, "b" to 2)
+
+        // Iterating over the Map.Entry elements of a map.
+        // "_" marks an unused component to avoid compiler warnings for unused variables
         var sum = 0
-        for ((key, value) in myMap) {
-            assertNotNull(key)
+        for ((_, value) in myMap) {
             sum += value
         }
+
         assertEquals(3, sum)
     }
 }
